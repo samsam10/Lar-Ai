811db22add6dabbf2a7823fb797527b7a94c73a4
Opticom-IT <dbriggshub@gmail.com>
2023-07-10 15:21:43 +0100

    responsiveness change

--- client/Dalle_Clone/src/Footer.jsx
+++ client/Dalle_Clone/src/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
-              <a href="/" class="flex items-center">
+              {/* <a href="/" class="flex items-center">
                   <img src='./src/assets/logo.svg' class="h-8 mr-3" alt="Logo" />
                   <span class="self-center text-2xl  font-semibold whitespace-nowrap dark:text-white">LariAi</span>
-              </a>
+              </a> */}
           </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
